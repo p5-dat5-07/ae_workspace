e8d5f9781f4d7405d162da5b68e34f744930b214
Jonas Kjellerup <jonas@hextech.dk>
2022-10-18 00:30:24 +0200

    Updated bindings with access to individual MidiEvents

--- evalpy/src/lib.rs
+++ evalpy/src/lib.rs
@@ -1,11 +1,19 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{stdout, Read, Write},
-    sync::Arc, collections::HashMap,
+    sync::Arc,
 };
 
-use pyo3::{prelude::*, exceptions::{PyIOError, PyBaseException}};
-use ss_analysis::{midly::{self, TrackEvent, Track}, plotters::prelude::*, Lens, TemporalSpace};
+use pyo3::{
+    exceptions::{PyBaseException, PyIOError},
+    prelude::*,
+};
+use ss_analysis::{
+    midly::{self, MidiMessage, Track, TrackEvent, TrackEventKind, MetaMessage},
+    plotters::prelude::*,
+    Lens, TemporalSpace,
+};
 
 // Error handling/conversion
 
@@ -58,8 +66,7 @@ impl From<Error> for PyErr {
         let msg = err.to_string();
         match err.kind {
             ErrorKind::IO => PyIOError::new_err(msg),
-            ErrorKind::Midly
-            | ErrorKind::Generic => PyBaseException::new_err(msg),
+            ErrorKind::Midly | ErrorKind::Generic => PyBaseException::new_err(msg),
         }
     }
 }
@@ -72,6 +79,8 @@ fn evalpy(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<MidiObject>()?;
     m.add_class::<TrackObject>()?;
     m.add_class::<TrackIterator>()?;
+    m.add_class::<MidiEventKind>()?;
+    m.add_class::<MidiEventObject>()?;
     m.add_class::<FeatureSpaceObject>()?;
     Ok(())
 }
@@ -83,7 +92,7 @@ type TrackLens = Lens<MidiLens, &'static Track<'static>>;
 
 #[pyclass(sequence)]
 struct MidiObject {
-    data: MidiLens,
+    inner: MidiLens,
 }
 
 #[pymethods]
@@ -94,57 +103,95 @@ impl MidiObject {
         let mut data = Vec::new();
         file.read_to_end(&mut data)?;
         Ok(
-            Lens::try_new::<Error>(data, |data| Ok(midly::Smf::parse(&data)?))
-                .map(|lens| Self { data: MidiLens::new(lens) })?,
+            Lens::try_new::<Error>(data, |data| Ok(midly::Smf::parse(&data)?)).map(|lens| {
+                Self {
+                    inner: MidiLens::new(lens),
+                }
+            })?,
         )
     }
-    
+
     pub fn headers(&self) -> HashMap<&'static str, String> {
         let mut map = HashMap::new();
-        
-        map.insert("format", format!("{:?}", self.data.header.format));
-        map.insert("timing", format!("{:?}", self.data.header.timing));
-        
+
+        map.insert("format", format!("{:?}", self.inner.header.format));
+        map.insert("timing", format!("{:?}", self.inner.header.timing));
+
         map
     }
-    
+
     fn __len__(&self) -> usize {
-        self.data.tracks.len()
+        self.inner.tracks.len()
     }
-    
+
     fn __getitem__(&self, i: isize) -> TrackObject {
         let i = if i < 0 {
-            self.data.tracks.len() as isize + i
+            self.inner.tracks.len() as isize + i
         } else {
             i
         };
-        
+
         TrackObject {
-            data: TrackLens::new_rc(self.data.clone(), |data| {
-                &data.tracks[i as usize]
-            })
+            inner: TrackLens::new_rc(self.inner.clone(), |data| &data.tracks[i as usize]),
         }
     }
-    
 }
 
-#[pyclass]
-// TODO implement sequencing
+#[pyclass(sequence)]
 struct TrackObject {
-    data: TrackLens,
+    inner: TrackLens,
 }
 
 #[pymethods]
 impl TrackObject {
     fn __iter__(slf: PyRef<'_, Self>) -> PyResult<Py<TrackIterator>> {
         let iter = TrackIterator {
-            iter: slf.data.clone_map(|track| track.iter()),
+            iter: slf.inner.clone_map(|track| track.iter()),
         };
         Py::new(slf.py(), iter)
     }
+
+    fn __len__(&self) -> usize {
+        self.inner.len()
+    }
+
+    fn __getitem__(&self, i: isize) -> MidiEventObject {
+        let i = if i < 0 {
+            self.inner.len() as isize + i
+        } else {
+            i
+        };
+
+        let event = &self.inner[i as usize];
+        MidiEventObject {
+            delta: event.delta.as_int(),
+            inner: match event.kind {
+                TrackEventKind::Midi { message, .. } => match message {
+                    MidiMessage::NoteOn { key, vel } => MidiEventData::NoteData {
+                        velocity: vel.as_int(),
+                        key: key.as_int(),
+                    },
+                    MidiMessage::NoteOff { key, .. } => MidiEventData::NoteData {
+                        velocity: 0,
+                        key: key.as_int(),
+                    },
+                    _ => MidiEventData::Other,
+                },
+                TrackEventKind::Meta(MetaMessage::TimeSignature(a, b, c, d)) => MidiEventData::TimeSignature([a,b,c,d]),
+                TrackEventKind::Meta(MetaMessage::EndOfTrack) => MidiEventData::EndOfTrack,
+                _ => MidiEventData::Other,
+            },
+        }
+    }
+
+    fn dbg_print(&self) {
+        println!("{:#?}", *(self.inner))
+    }
     
     fn feature_space(&self) -> FeatureSpaceObject {
-        FeatureSpaceObject { inner: TemporalSpace::new(*self.data) }
+        FeatureSpaceObject {
+            inner: TemporalSpace::new(*self.inner),
+        }
     }
 }
 
@@ -155,13 +202,64 @@ struct TrackIterator {
 
 #[pymethods]
 impl TrackIterator {
-    fn __item__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
         slf
     }
-    
+
     fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<u32> {
         let a = slf.iter.next();
-        a.map(|e| {e.delta.as_int()})
+        a.map(|e| e.delta.as_int())
+    }
+}
+
+#[pyclass]
+#[derive(Clone, Copy)]
+enum MidiEventKind {
+    Note,
+    Tempo,
+    TimeSignature,
+    EndOfTrack,
+    Other,
+}
+
+enum MidiEventData {
+    NoteData { velocity: u8, key: u8 },
+    TimeSignature([u8; 4]),
+    EndOfTrack,
+    Other,
+}
+
+#[pyclass]
+struct MidiEventObject {
+    #[pyo3(get)]
+    delta: u32,
+    inner: MidiEventData,
+}
+
+#[pymethods]
+impl MidiEventObject {
+    #[getter]
+    fn velocity(&self) -> Option<u8> {
+        match self.inner {
+            MidiEventData::NoteData { velocity, .. } => Some(velocity),
+            _ => None,
+        }
+    }
+
+    #[getter]
+    fn key(&self) -> Option<u8> {
+        match self.inner {
+            MidiEventData::NoteData { key, .. } => Some(key),
+            _ => None,
+        }
+    }
+
+    #[getter]
+    fn time_signature(&self) -> Option<[u8; 4]> {
+        match self.inner {
+            MidiEventData::TimeSignature(signature) => Some(signature),
+            _ => None,
+        }
     }
 }
 
@@ -172,18 +270,18 @@ struct FeatureSpaceObject {
 
 #[pymethods]
 impl FeatureSpaceObject {
-    
     fn draw_ssm(&self, path: &str, scale: f32) -> PyResult<()> {
         let size = (self.inner.size() as f32 * scale) as u32;
         let root = BitMapBackend::new(path, (size, size)).into_drawing_area();
-        root.fill(&WHITE).map_err(|_| PyBaseException::new_err("Unexpected error drawing SSM"))?;
-        
-        let size = self.inner.size();
-        let mut chart = ChartBuilder::on(&root).build_cartesian_2d(0..size, size..0)
+        root.fill(&WHITE)
             .map_err(|_| PyBaseException::new_err("Unexpected error drawing SSM"))?;
-        
+
+        let size = self.inner.size();
+        let mut chart = ChartBuilder::on(&root)
+            .build_cartesian_2d(0..size, size..0)
+            .map_err(|_| PyBaseException::new_err("Unexpected error drawing SSM"))?;
+
         chart.draw_series(self.inner.draw()).unwrap();
         Ok(())
     }
-    
 }
